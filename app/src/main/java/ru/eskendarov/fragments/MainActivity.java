3828d015a5e664452dce7807fa9f238f3af103f4
Eskendarov <envereskendarov@gmail.com>
2019-01-21 15:30:55 +0300

    add fragments

--- app/src/main/java/ru/eskendarov/fragments/MainActivity.java
+++ app/src/main/java/ru/eskendarov/fragments/MainActivity.java
@@ -1,14 +1,42 @@
 package ru.eskendarov.fragments;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private final String LOG_TAG = "myLogs";
+
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "MainActivity onCreate");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "MainActivity onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "MainActivity onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "MainActivity onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "MainActivity onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MainActivity onDestroy");
     }
 
 }
